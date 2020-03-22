8ba9233eb50c87639533e66fdde9904acc934f2d
santiago villalobos <55002307+SanjeVicor@users.noreply.github.com>
2020-03-21 19:07:31 -0600

    Add files via upload

---
+++ lands/load.py
@@ -0,0 +1,24 @@
+import os
+import re
+import shutil
+
+def get_images():
+    path = os.path
+    data = os.listdir('lands')
+    paths = []
+    print(data)
+    for e in data:
+        print(e)
+        if re.fullmatch(r".+\x2e(jpg|png)", e):
+            paths.append(os.path.abspath(os.path.join("lands/",e)))
+
+    print(paths)
+    return paths
+
+def copy_image(file_path,file_name):
+    file_format = file_path.split("/")[-1]
+    file_format = file_format.split(".")[-1]
+    file_name += f".{file_format}"
+    ok = shutil.copyfile(file_path,os.path.abspath(os.path.join(f"lands/{file_name}")))
+    if ok :
+        return ok
